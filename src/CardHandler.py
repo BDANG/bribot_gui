005ed32d9becb4be599d7406bbf8fdef1e312c56
Brian Dang <heybdang@gmail.com>
2018-02-10 00:18:03 -0500

    filled out the add-card popup

--- src/CardHandler.py
+++ src/CardHandler.py
@@ -4,6 +4,8 @@ from tkinter import ttk
 from tkinter.ttk import *
 
 class CardHandler:
+    MIN_YEAR = 2018
+    MAX_YEAR = 2026
     def __init__(self, tree):
         self.tree = tree
         self.PROMPT_DELETE = True
@@ -21,71 +23,104 @@ class CardHandler:
         popup = Toplevel()
         popup.title("Add a Card")
 
+        inputframe = Frame(popup)
+        inputframe.grid(row=1, column=1, columnspan=2)
+
+
         #####################
         ##### Name Entry
-        Label(popup, text="Name: ").grid(row=1, column=1, sticky=W)
-        nameEntry = Entry(popup, name="entry_name", width=10)
+        Label(inputframe, text="Name: ").grid(row=1, column=1, sticky=W)
+        nameEntry = Entry(inputframe, name="entry_name", width=10)
         nameEntry.grid(row=1, column=2, sticky=W)
 
         #####################
         #### Email Entry
-        Label(popup, text="Email: ").grid(row=2, column=1, sticky=W)
-        emailEntry = Entry(popup, name="entry_email", width=10)
+        Label(inputframe, text="Email: ").grid(row=2, column=1, sticky=W)
+        emailEntry = Entry(inputframe, name="entry_email", width=10)
         emailEntry.grid(row=2, column=2, sticky=W)
 
         #####################
         #### Phone Entry: WIP validate format?
-        Label(popup, text="Phone: ").grid(row=3, column=1, sticky=W)
-        phoneEntry = Entry(popup, name="entry_phone", width=10)
+        Label(inputframe, text="Phone: ").grid(row=3, column=1, sticky=W)
+        phoneEntry = Entry(inputframe, name="entry_phone", width=10)
         phoneEntry.grid(row=3, column=2, sticky=W)
 
         #####################
         #### Address Entry
-        Label(popup, text="Address: ").grid(row=4, column=1, sticky=W)
-        address1Entry = Entry(popup, name="entry_address1", width=10)
+        Label(inputframe, text="Address: ").grid(row=4, column=1, sticky=W)
+        address1Entry = Entry(inputframe, name="entry_address1", width=10)
         address1Entry.grid(row=4, column=2, sticky=W)
 
-        Label(popup, text="Apt / Unit: ").grid(row=5, column=1, sticky=W)
-        addressAptEntry = Entry(popup, name="entry_apt", width=10)
+        Label(inputframe, text="Apt / Unit: ").grid(row=5, column=1, sticky=W)
+        addressAptEntry = Entry(inputframe, name="entry_apt", width=10)
         addressAptEntry.grid(row=5, column=2, sticky=W)
 
-        Label(popup, text="Address: ").grid(row=6, column=1, sticky=W)
-        address2Entry = Entry(popup, name="entry_address2", width=10)
+        Label(inputframe, text="Address 2: ").grid(row=6, column=1, sticky=W)
+        address2Entry = Entry(inputframe, name="entry_address2", width=10)
         address2Entry.grid(row=6, column=2, sticky=W)
 
         #####################
         #### Zip Entry
-        Label(popup, text="Zip: ").grid(row=7, column=1, sticky=W)
-        zipEntry = Entry(popup, name="entry_zip", width=10)
+        Label(inputframe, text="Zip: ").grid(row=7, column=1, sticky=W)
+        zipEntry = Entry(inputframe, name="entry_zip", width=10)
         zipEntry.grid(row=7, column=2, sticky=W)
 
         #####################
         #### City Entry
-        Label(popup, text="City: ").grid(row=8, column=1, sticky=W)
-        cityEntry = Entry(popup, name="entry_city", width=10)
+        Label(inputframe, text="City: ").grid(row=8, column=1, sticky=W)
+        cityEntry = Entry(inputframe, name="entry_city", width=10)
         cityEntry.grid(row=8, column=2, sticky=W)
 
         #####################
         #### State Entry
-        Label(popup, text="State: ").grid(row=9, column=1, sticky=W)
-        stateEntry = Entry(popup, name="entry_state", width=10)
+        Label(inputframe, text="State: ").grid(row=9, column=1, sticky=W)
+        stateEntry = Entry(inputframe, name="entry_state", width=10)
         stateEntry.grid(row=9, column=2, sticky=W)
 
         #####################
         #### Country Entry
-        Label(popup, text="Country: ").grid(row=10, column=1, sticky=W)
-        countryBox = Combobox(popup,
+        Label(inputframe, text="Country: ").grid(row=10, column=1, sticky=W)
+        countryBox = Combobox(inputframe,
                               width=10,
                               name="combobox_country",
                               state="readonly",
                               values=["USA", "Canada"])
         countryBox.grid(row=10, column=2, sticky=W)
 
-        Separator(popup, orient=VERTICAL).grid(row=1, column=3, rowspan=10, sticky="ns")
+        Separator(inputframe, orient=VERTICAL).grid(row=1, column=3, rowspan=10, sticky="ns", padx=3, pady=2)
 
-        Label(popup, text="Card Num: ").grid(row=1, column=4, sticky=W)
-        cardnumEntry = Entry(popup, name="entry_cardnum", width=10)
+        #####################
+        #### Card Num Entry
+        Label(inputframe, text="Card Num: ").grid(row=1, column=4, sticky=W)
+        cardnumEntry = Entry(inputframe, name="entry_cardnum", width=10)
         cardnumEntry.grid(row=1, column=5, sticky=W)
+
+        #####################
+        #### Card Month Entry
+        cardmonthBox = Combobox(inputframe,
+                                width=3,
+                                name="combobox_cardmonth",
+                                state="readonly",
+                                values=["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+                                )
+        cardmonthBox.grid(row=2, column=4, sticky=W)
+
+        #####################
+        #### Card Year Entry
+        cardyearBox = Combobox(inputframe,
+                               width=4,
+                               name="combobox_cardyear",
+                               state="readonly",
+                               values=[str(year) for year in range(self.MIN_YEAR, self.MAX_YEAR+1)])
+        cardyearBox.grid(row=2, column=5, sticky=W)
+
+        #####################
+        #### CVV Entry
+        Label(inputframe, text="CVV: ").grid(row=3, column=4, sticky=W)
+        cvvEntry = Entry(inputframe, name="entry_cvv", width=4)
+        cvvEntry.grid(row=3, column=5, sticky=W)
+
+
 
 
 
@@ -104,8 +139,8 @@ class CardHandler:
                                                       nameEntry.get(),
                                                       popup
                                                       )
-               ).grid(row=11, column=1)
-        Button(popup, text="Cancel", command=popup.destroy).grid(row=11, column=2)
+               ).grid(row=2, column=1)
+        Button(popup, text="Cancel", command=popup.destroy).grid(row=2, column=2)
 
     def _confirm_delete(self, itemID):
         # init popup
